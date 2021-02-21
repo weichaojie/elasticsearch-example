1fc2a89c1dbc93816c69f886ca51092130280acd
weichaojie <18971302695@189.cn>
2021-02-21 15:46:58 +0800

    add ignore confiration

--- src/test/java/org/bboss/elasticsearchtest/mytest.java
+++ src/test/java/org/bboss/elasticsearchtest/mytest.java
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
 public class mytest{
 	public static void main(String[] args) {
 		System.out.print("my test output");
 		for (int i = 0; i <20; ++i){
 			System.out.println(i);
+=======
+public class mytest {
+	public static void main(String[] args) {
+		System.out.print("my test output");
+		for (int i = 0; i <20; ++i){
+			System.out.print(i);
+>>>>>>> add ignore confiration
 		}
 	}
 }
