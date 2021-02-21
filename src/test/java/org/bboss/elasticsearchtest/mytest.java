952e1021c4dc881494f56f47aa341c958ebd678f
weichaojie <18971302695@189.cn>
2021-02-21 15:50:43 +0800

    delete temp file (.class)

--- src/test/java/org/bboss/elasticsearchtest/mytest.java
+++ src/test/java/org/bboss/elasticsearchtest/mytest.java
@@ -4,13 +4,5 @@ public class mytest{
 		System.out.print("my test output");
 		for (int i = 0; i <20; ++i){
 			System.out.println(i);
-=======
-public class mytest {
-	public static void main(String[] args) {
-		System.out.print("my test output");
-		for (int i = 0; i <20; ++i){
-			System.out.print(i);
->>>>>>> add ignore confiration
 		}
 	}
-}
