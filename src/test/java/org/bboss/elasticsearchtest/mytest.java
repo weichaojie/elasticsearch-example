ea37bda5356c397cab2dd3793f64a6bb07ab18da
weichaojie <18971302695@189.cn>
2021-02-21 13:46:54 +0800

    add a launch settings file

--- src/test/java/org/bboss/elasticsearchtest/mytest.java
+++ src/test/java/org/bboss/elasticsearchtest/mytest.java
@@ -1,7 +1,5 @@
-import java.util.List;
-import java.util.Map;
 
-public static void main(String[] args) {
+void main(String[] args) {
 	System.out.print("my test output");
 	for (int i = 0; i <20; ++i){
 		System.out.print("%d", i);
