b8b5904d1fc823ca37b46864b4190c7783556979
weichaojie <chaojie.weichaojie@alibaba-inc.com>
2021-02-22 21:12:07 +0800

    fix compile error

--- src/test/java/org/bboss/elasticsearchtest/mytest.java
+++ src/test/java/org/bboss/elasticsearchtest/mytest.java
@@ -1,7 +1,8 @@
-
-void main(String[] args) {
-	System.out.print("my test output");
-	for (int i = 0; i <20; ++i){
-		System.out.print("%d", i);
+public class mytest{
+	public static void main(String[] args) {
+		System.out.print("my test output");
+		for (int i = 0; i <20; ++i){
+			System.out.println(i);
+		}
 	}
 }
